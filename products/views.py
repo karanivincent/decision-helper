209e8fd381ba0ccb61ccd42a53ee785e036c0e1c
karanivincent <trevorvinnie@gmail.com>
2019-05-05 20:22:06 +0300

    finished on accounts

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
+from products.forms import ProductForm
+from products.models import Product
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 
 # Create your views here.
+class CreateProductView(CreateView):
+    redirect_field_name = 'products/product_form.html'
+    form_class = ProductForm
+    model = Product
